65aa0d94d18a9f640eed1573c82d8f2c85b6719e
Stefan Kogelnik <stefan.kogelnik@edu.fh-joanneum.at>
2017-02-02 09:17:06 +0100

    File renamed

---
+++ Resifo/app/src/main/java/at/fh/swengb/resifo/InputOneActivity.java
@@ -0,0 +1,17 @@
+package at.fh.swengb.resifo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Stefan on 02.02.2017.
+ */
+
+public class InputOneActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.input1_page);
+    }
+}
--- Resifo/app/src/main/java/at/fh/swengb/resifo/InstructionActivity.java
+++ Resifo/app/src/main/java/at/fh/swengb/resifo/InstructionActivity.java
@@ -1,7 +1,9 @@
 package at.fh.swengb.resifo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class InstructionActivity extends Activity{
@@ -10,6 +12,12 @@ public class InstructionActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.instruction_page);
+    }
+
+    public void onUnderstand(View view) {
+        Intent myIntent = new Intent(this, InputOneActivity.class);
+
+        this.startActivity(myIntent);
     }
 
 }
