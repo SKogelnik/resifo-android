6b17f4036008e89a63edc66a66724b53ff159325
Fabian Schopper <fabian.schopper@edu-fh.joanneum.at>
2017-02-02 11:18:49 +0100

    textfield added

--- Resifo/app/src/main/java/at/fh/swengb/resifo/InstructionActivity.java
+++ Resifo/app/src/main/java/at/fh/swengb/resifo/InstructionActivity.java
@@ -4,14 +4,32 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class InstructionActivity extends Activity{
+
+    TextView instruction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.instruction_page);
+
+        instruction = (TextView)findViewById(R.id.instructionText  );
+        instruction.setText(
+
+                        "ANLEITUNG \n" +
+                        "ANLEITUNG \n" +
+                        "ANLEITUNG \n" +
+                        "ANLEITUNG \n" +
+                        "ANLEITUNG \n" +
+                        "ANLEITUNG \n" +
+                        "ANLEITUNG \n"
+
+        );
+
+
     }
 
     public void onUnderstand(View view) {
