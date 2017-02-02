a166a0c7452da49de245097205262f79bcc1fd95
Stefan Kogelnik <stefan.kogelnik@edu.fh-joanneum.at>
2017-02-02 11:06:58 +0100

    Testtext added

--- Resifo/app/src/main/java/at/fh/swengb/resifo/AboutActivity.java
+++ Resifo/app/src/main/java/at/fh/swengb/resifo/AboutActivity.java
@@ -2,12 +2,19 @@ package at.fh.swengb.resifo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class AboutActivity extends Activity{
+    TextView tvImpressum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.impressum);
+
+        tvImpressum = (TextView)findViewById(R.id.tvIMPRESSUM);
+        tvImpressum.setText("TESTTEXT");
     }
 }
