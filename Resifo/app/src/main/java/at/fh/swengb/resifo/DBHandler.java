a0aded770bd756ff6c0f9a544a038670b72faf31
Fabian Schopper <fabian.schopper@edu-fh.joanneum.at>
2017-02-03 15:55:14 +0100

    DBHandler added

---
+++ Resifo/app/src/main/java/at/fh/swengb/resifo/DBHandler.java
@@ -0,0 +1,82 @@
+package at.fh.swengb.resifo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+/**
+ * Created by fabian on 03.02.17.
+ */
+
+public class DBHandler extends SQLiteOpenHelper {
+
+
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+    // Database Name
+    private static final String DATABASE_NAME = "database";
+    // ID
+    private static final String KEY_ID = "id";
+
+    // rows
+    private static final String VORNAME = "vorname";
+    private static final String NACHNAME = "nachname";
+    private static final String NACHNAMEALT = "nachnamealt";
+    private static final String GEBURTSTDATUM ="geburtsdatum";
+    private static final String GEBURTSORT ="geburtsort";
+    private static final String GESCHLECHT ="geschlecht";
+    private static final String RELIGION ="religion";
+    private static final String FAMILIENSTAND = "familienstand";
+    private static final String NATION = "nation";
+    private static final String ZMR = "zmr";
+
+    private static final String DOKNUMMER = "dokumentnummer";
+    private static final String AUSSTELLDATUM ="ausstelldatum";
+    private static final String AUSSTELLBEHOERDE ="ausstellbehoerde";
+
+    private static final String STRASSE = "strasse";
+    private static final String HAUSNUMMER = "hausnummer";
+    private static final String PLZ = "postleitzahl";
+    private static final String ORT = "ort";
+
+
+    public DBHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);}
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_DATABASE_TABLE ="CREATE TABLE"+DATABASE_NAME+"("+KEY_ID+"INTEGER PRIMARY KEY,"
+                +VORNAME+"TEXT,"
+                +NACHNAME+"TEXT,"
+                +NACHNAMEALT+"TEXT,"
+                +GEBURTSTDATUM+"TEXT,"
+                +GEBURTSORT+"TEXT,"
+                +GESCHLECHT+"TEXT,"
+                +RELIGION+"TEXT,"
+                +FAMILIENSTAND+"TEXT,"
+                +NATION+"TEXT,"
+                +ZMR+"TEXT,"
+                +DOKNUMMER+"TEXT,"
+                +AUSSTELLDATUM+"TEXT,"
+                +AUSSTELLBEHOERDE+"TEXT,"
+                +STRASSE+"TEXT,"
+                +HAUSNUMMER+"TEXT,"
+                +PLZ+"TEXT,"
+                +ORT+"TEXT,"
+                +")";
+
+
+        db.execSQL(CREATE_DATABASE_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + DATABASE_NAME);
+        // Creating tables again
+        onCreate(db);
+
+    }
+}
